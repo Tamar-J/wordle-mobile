0fdbc272026f32c816faf25888fcc8ecddf2db6a
Tamar-J <tamar_judson@hotmail.com>
2022-07-13 14:51:18 -0300

    feat: game board logic

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
+import { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { colors } from './src/constants'
+import { colors, CLEAR, ENTER } from './src/constants'
 import Keyboard from './src/components/Keyboard'
 
 const NUMBER_OF_TRIES = 6;
@@ -10,7 +11,51 @@ export default function App() {
   const word = "hello"
   const letters = word.split('')
 
-  const rows = new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill("a"))
+  const [rows, setRows] = useState(
+    Array(NUMBER_OF_TRIES).fill("").map(e => Array(letters.length).fill(""))
+  )
+
+  const [currRow, setCurrRow] = useState(0)
+  const [currCol, setCurrCol] = useState(0)
+
+  const handleKeyPress = (key) => {
+    if (key === CLEAR) {
+      const prevColumn = currCol - 1
+
+      if (prevColumn >= 0) {
+        setRows(prevRows => {  
+          prevRows[currRow][prevColumn] = ""
+          return prevRows
+        })
+        setCurrCol(prevCol => prevCol - 1)
+      }
+      return
+    }
+
+    const wordLength = rows[0].length
+
+    if (key === ENTER) {
+      const currTry = currRow + 1
+
+      if (currTry < NUMBER_OF_TRIES && currCol === wordLength) {
+        setCurrRow(prevRow => prevRow + 1)
+        setCurrCol(0)
+      }
+      return
+    }
+
+    if (currCol < wordLength) {
+      setRows(prevRows => {  
+        prevRows[currRow][currCol] = key
+        return prevRows
+      })
+      setCurrCol(prevCol => prevCol + 1)
+    }
+  }
+
+  const isCellActive = (row, col) => {
+    return row === currRow && col === currCol
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -19,18 +64,29 @@ export default function App() {
       <Text style={styles.title}>WORDLE</Text>
 
       <ScrollView style={styles.map}>
-        {rows.map(row => (
-          <View style={styles.row}>
-            {row.map(cell => (
-              <View style={styles.cell} > 
-                <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
+        {rows.map((row, indexY) => (
+          <View key={`row-${indexY}`} style={styles.row}>
+            {row.map((cell, indexX) => (
+              <View 
+                key={`cell-${indexY}-${indexX}`}
+                style={[
+                  styles.cell,
+                  { 
+                    borderColor: isCellActive(indexY, indexX)
+                    ? colors.grey
+                    : colors.darkgrey
+                  }
+                ]} 
+              > 
+                <Text style={styles.cellText}>
+                {cell.toUpperCase()}</Text>
               </View>
             ))}
           </View>
 
         ))}
       </ScrollView>
-      <Keyboard/>
+      <Keyboard onKeyPressed={handleKeyPress}/>
     </SafeAreaView>
   );
 }
